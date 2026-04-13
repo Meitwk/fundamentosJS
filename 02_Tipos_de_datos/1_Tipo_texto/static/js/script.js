2f8c560b9ddd7183596a2fef8392fd7fe721f83c
Catalina Mena <catalinamena@liceovvh.cl>
2026-04-13 13:56:12 -0400

    yata

---
+++ 02_Tipos_de_datos/1_Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexión con js correcta...");
+/*
+Dato: Tipo texto(string)
+Concatenacion de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre = "Catalina";
+const apellido = "Mena";
+//Unimos ambas constantes de un texto extra.
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
